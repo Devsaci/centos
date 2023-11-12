bc8c2ef8ed6bd883f4335e2e77ff6155a87f7306
saci zakaria <sacideveloppeur@gmail.com>
2023-11-12 12:41:37 +0100

    $ vagrant status

--- readme.cmd
+++ readme.cmd
@@ -2,3 +2,12 @@
 $ vagrant init eurolinux-vagrant/centos-stream-9
 $ cat Vagrantfile 
 $ vagrant up
+$ vagrant status
+Current machine states:
+
+default                   running (virtualbox)
+
+The VM is running. To stop this VM, you can run `vagrant halt` to  
+shut it down forcefully, or you can run `vagrant suspend` to simply
+suspend the virtual machine. In either case, to restart it again,  
+simply run `vagrant up`.
